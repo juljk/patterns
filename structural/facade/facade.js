692db1b639052d921f9b6498fdc84d600c7bb062
juljk <julikjulik22@gmail.com>
2020-02-06 22:49:20 +0200

    facade

--- structural/facade/facade.js
+++ structural/facade/facade.js
@@ -1,12 +1,48 @@
 // EXAMPLE #2
 class Conveyer {
     setBody() { console.log('Body set!'); };
-    setBody() { console.log(''); };
-    setBody() { console.log(); };
-    setBody() { console.log(); };
-    setBody() { console.log(); };
-    setBody() { console.log(); };
-    setBody() { console.log(); };
-    setBody() { console.log(); };
-    setBody() { console.log(); };
-}
+    getEngine() { console.log('Dismantle Engine!'); };
+    setEngine() { console.log('Engine set!'); };
+    getInterior() { console.log('Update interior!'); };
+    setInterior() { console.log('Exterior added!'); };
+    setExterior() { console.log('Added exterior!'); };
+    setWheels() { console.log('Wheels!'); };
+    addElectronics() { console.log('Added electronics!'); };
+    paint() { console.log('Car printed!'); };
+};
+
+class ConveyerFacade {
+    constructor(car) {
+        this.car = car;
+    }
+
+    assembleCar() {
+        this.car.setBody();
+        this.car.getEngine();
+        this.car.setEngine();
+        this.car.getInterior();
+        this.car.setInterior();
+        this.car.setExterior();
+        this.car.setWheels();
+        this.car.addElectronics();
+        this.car.paint();
+    }
+
+    changeEngine() {
+        this.car.getEngine();
+        this.car.setEngine();
+    }
+
+    changeInterior() {
+        this.car.getInterior();
+        this.car.setInterior();
+    }
+};
+
+const conveyer = new ConveyerFacade(new Conveyer());
+
+let car = conveyer.assembleCar();
+car = conveyer.changeEngine();
+car = conveyer.changeInterior();
+
+console.log(car);
